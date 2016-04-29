73aca2ec59c702d28b8af2821457413458e3a9dd
momokoishimura <applemelody03@gmail.com>
2016-04-29 13:49:28 +0000

    add kaminari gem

--- Gemfile
+++ Gemfile
@@ -49,3 +49,5 @@ end
 
 gem 'validates_timeliness', '~> 3.0'
 
+gem 'kaminari'
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
   
+  def index
+    @microposts = @user.microposts.page(params[:page])
+  end
+  
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
